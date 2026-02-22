eaf30685740f060954339354fdb2d900dc737562
oblepihaha-coder <feromonhik1@gmail.com>
2026-02-22 17:37:18 +0200

    Init project

---
+++ ConsoleApp1.Tests/NewDirectory1/CircleTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xunit;
+using ConsoleApp1.NewDirectory1;
+
+namespace CircleTests
+{
+    public class CircleUnitTests
+    {
+        [Fact]
+        public void GetArea_PositiveRadius_ReturnsCorrectArea()
+        {
+            double radius = 5;
+            Circle circle = new Circle(radius);
+            double expectedArea = Math.PI * radius * radius;
+            double actualArea = circle.GetArea();
+            Assert.Equal(expectedArea, actualArea, 5); 
+        }
+        [Fact]
+        public void Constructor_NegativeRadius_ThrowsArgumentException()
+        {
+            double radius = -3;
+            Assert.Throws<ArgumentException>(() => new Circle(radius));
+        }
+        [Fact]
+        public void GetCircumference_ZeroRadius_ReturnsZero()
+        {
+            double radius = 0;
+            Circle circle = new Circle(radius);
+            double circumference = circle.GetCircumference();
+            Assert.Equal(0, circumference);
+        }
+    }
+}
---
+++ ConsoleApp1/NewDirectory1/Circle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp1.NewDirectory1;
+
+public class Circle
+{
+    public double Radius;
+
+    public Circle(double radius)
+    {
+        Radius = radius;
+    }
+
+    public double GetArea()
+    {
+        return Math.PI * Radius * Radius;
+    }
+
+    public double GetCircumference()
+    {
+        return 2 * Math.PI * Radius;
+    }
+}
