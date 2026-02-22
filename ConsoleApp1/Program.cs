888e826311ea9da103a46895461d9d27897131b6
oblepihaha-coder <feromonhik1@gmail.com>
2026-02-22 17:37:18 +0200

    Init project

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using ConsoleApp1.NewDirectory1;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введіть радіус кола: ");
+        double radius = double.Parse(Console.ReadLine());
+
+        Circle circle = new Circle(radius);
+
+        Console.WriteLine("Площа кола: " + circle.GetArea());
+        Console.WriteLine("Довжина кола: " + circle.GetCircumference());
+    }
+}
